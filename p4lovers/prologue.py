56743a4bfc2771778ca856d85a698487f4fb1b29
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2022-07-11 13:29:58 +0800

    use doctest instead

--- p4lovers/prologue.py
+++ p4lovers/prologue.py
@@ -9,24 +9,36 @@ def factorial(n: int):
 
 def permutation(n: int, k: int):
     '''P(n, k) = n!/(n-k)!
+    
+    >>> permutation(10, 0)
+    1
+    >>> permutation(10, 2)
+    90
+    >>> permutation(10, 10)
+    3628800
+    >>> permutation(1000, 2)
+    999000
     '''
     return factorial(n) // factorial(n - k)
-
-def test_permutation():
-    assert permutation(10, 0) == 1
-    assert permutation(10, 2) == 90
-    assert permutation(10, 10) == 3628800
-    assert permutation(1000, 2) == 999000
 
 
 def combination(n: int, k: int):
     '''C(n, k) = P(n, k)/k! = n!/((n-k)! * k!)
+    
+    >>> combination(10, 0)
+    1
+    >>> combination(10, 2)
+    45
+    >>> combination(10, 10)
+    1
+    >>> combination(1000, 2)
+    499500
+    >>> combination(1000, 998)
+    499500
     '''
     return permutation(n, k) // factorial(k)
 
-def test_combination():
-    assert combination(10, 0) == 1
-    assert combination(10, 2) == 45
-    assert combination(10, 10) == 1
-    assert combination(1000, 2) == 499500
-    assert combination(1000, 998) == 499500
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
